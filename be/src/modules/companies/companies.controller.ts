31baaf7f723a556a2adeea5adac9e0a31bb7f24e
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-04-07 10:40:59 +0700

    feat: add route to update company for recruiter and write Swagger documentation.

--- be/src/modules/companies/companies.controller.ts
+++ be/src/modules/companies/companies.controller.ts
@@ -1,7 +1,117 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { User } from '@supabase/supabase-js';
+import { Request } from 'express';
+import { FileValidationDecorator, Roles } from 'src/libs/common/decorators';
+import { RoleAuthGuard, SupabaseGuard } from 'src/libs/common/guards';
+import { RoleEnum } from 'src/libs/common/utils';
+import { UpdateCompanyDto } from 'src/modules/companies/dtos';
 import { CompaniesService } from './companies.service';
 
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Cập nhật công ty',
+    description:
+      'Đường dẫn này dùng để cập nhật thông tin công ty của nhà tuyển dụng.',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        success: true,
+        message: 'Đã cập nhật thành công thông tin mới về công ty của bạn.',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: {
+        message:
+          'Bạn cần cung cấp token để truy cập vào tài nguyên của đường dẫn này.',
+        error: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
+  @ApiBody({
+    type: UpdateCompanyDto,
+    description: 'Dữ liệu cần gửi đi để cập nhật thông tin cho công ty.',
+  })
+  @ApiForbiddenResponse({
+    examples: {
+      example1: {
+        summary: 'Không phải role recruiter thì bị cấm.',
+        value: {
+          message:
+            'Bạn không có quyền truy cập vào tài nguyên của đường dẫn này.',
+          error: 'Forbidden',
+          statusCode: 403,
+        },
+      },
+      example2: {
+        summary:
+          'Có role recruiter nhưng không phải cập nhật công ty của chính mình.',
+        value: {
+          message: 'Bạn chỉ có thể chỉnh sửa công ty của chính mình.',
+          error: 'Forbidden',
+          statusCode: 403,
+        },
+      },
+    },
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Mã định danh (ID) duy nhât của công ty',
+    example: '2180647a-d0e5-4062-a4a1-28de8bdf539e',
+  })
+  @ApiNotFoundResponse({
+    schema: {
+      example: {
+        message:
+          "Không tìm thấy công ty có id '2180647a-d0e5-4062-a4a1-28de8bdf5392' trong hệ thống.",
+        error: 'Not Found',
+        statusCode: 404,
+      },
+    },
+  })
+  @UseInterceptors(AnyFilesInterceptor())
+  @UseGuards(SupabaseGuard, RoleAuthGuard)
+  @Roles(RoleEnum.RECRUITER)
+  async updateCompany(
+    @Param('id', ParseUUIDPipe) companyId: string,
+    @Body() updateCompanyDto: UpdateCompanyDto,
+    @Req() request: Request,
+    @FileValidationDecorator() files?: Express.Multer.File[],
+  ) {
+    const userId = (request.user as User).id;
+
+    return this.companiesService.handleUpdateCompany(
+      companyId,
+      updateCompanyDto,
+      userId,
+      files?.find((file) => file.fieldname === 'logoFile'),
+    );
+  }
 }
--- be/src/modules/companies/companies.service.ts
+++ be/src/modules/companies/companies.service.ts
@@ -1,4 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Companies, Recruiters } from '@prisma/client';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { InjectSupabaseClient } from 'nestjs-supabase-js';
+import { UpdateCompanyDto } from 'src/modules/companies/dtos';
+import { UploadsService } from 'src/modules/uploads/uploads.service';
 
 @Injectable()
-export class CompaniesService {}
+export class CompaniesService {
+  constructor(
+    @InjectSupabaseClient('adminClient')
+    private readonly adminSupabaseClient: SupabaseClient,
+    @InjectSupabaseClient('anonClient')
+    private readonly anonSupabaseClient: SupabaseClient,
+    private readonly uploadsService: UploadsService,
+  ) {}
+
+  public handleUpdateCompany = async (
+    companyId: string,
+    updateCompanyDto: UpdateCompanyDto,
+    userId: string,
+    logoFile?: Express.Multer.File,
+  ) => {
+    try {
+      const { data: recruiter } = await this.anonSupabaseClient
+        .from('Recruiters')
+        .select('*')
+        .eq('UserID', userId)
+        .maybeSingle<Recruiters>();
+
+      if (!recruiter)
+        throw new NotFoundException(
+          `Không tìm thấy thông tin về hồ sơ nhà ứng tuyển của bạn. Vui lòng liên hệ với quản trị viên.`,
+        );
+
+      const { data: company } = await this.anonSupabaseClient
+        .from('Companies')
+        .select('*, CompanyLocations(*, Recruiters(*))')
+        .eq('ID', companyId)
+        .maybeSingle<any>();
+
+      if (!company)
+        throw new NotFoundException(
+          `Không tìm thấy công ty có id '${companyId}' trong hệ thống.`,
+        );
+
+      const recruiterIds = company.CompanyLocations.flatMap(
+        (cl: any) => cl.Recruiters,
+      ).map((r: any) => r.ID);
+
+      if (!recruiterIds.includes(recruiter.ID))
+        throw new ForbiddenException(
+          'Bạn chỉ có thể chỉnh sửa công ty của chính mình.',
+        );
+
+      let logoFileUrl: string = '';
+
+      if (logoFile) {
+        const { url } = await this.uploadsService.uploadFile(logoFile, 'files');
+
+        if (url) logoFileUrl = url;
+      }
+
+      const { error } = await this.adminSupabaseClient
+        .from('Companies')
+        .update([
+          {
+            ...updateCompanyDto,
+            ...(logoFileUrl && { LogoUrl: logoFileUrl }),
+          },
+        ])
+        .eq('ID', companyId);
+
+      if (error) {
+        console.error(error);
+
+        throw new InternalServerErrorException(
+          'Đã xảy ra lỗi khi cập nhật thông tin cho công ty của bạn. Vui lòng thử lại sau.',
+        );
+      }
+
+      return {
+        success: true,
+        message: 'Đã cập nhật thành công thông tin mới về công ty của bạn.',
+      };
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+}
